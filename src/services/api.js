11c8880f02d54c543d411410029e3b4b5b0664fb
Enrique Rodríguez Angulo <152347912+Cielz04@users.noreply.github.com>
2025-11-11 10:03:43 -0600

    update
    ahora el metodo es mas descriptivo con los errores

--- src/services/api.js
+++ src/services/api.js
@@ -47,6 +47,24 @@ export const getSorteoById = async (id) => {
 };
 
 // Apartar números para un sorteo
+
+  // Funcion anterior
+    // export const apartarNumeros = async (sorteoId, numeros, token) => {
+    //   const bodyData = { numerosBoletos: numeros };
+    //   const res = await fetch(`${API_URL}/${RAFFLES_PATH}/${sorteoId}/tickets`, {
+    //     method: "POST",
+    //     headers: {
+    //       "Content-Type": "application/json",
+    //       "Authorization": `Bearer ${token}`,
+    //     },
+    //     body: JSON.stringify(bodyData),
+    //   });
+    //   if (!res.ok) {
+    //     throw new Error(`Error al apartar números: ${res.status}`);
+    //   }
+    //   const data = await res.json();
+    //   return data;
+    // }
 export const apartarNumeros = async (sorteoId, numeros, token) => {
   const bodyData = { numerosBoletos: numeros };
   const res = await fetch(`${API_URL}/${RAFFLES_PATH}/${sorteoId}/tickets`, {
@@ -57,12 +75,17 @@ export const apartarNumeros = async (sorteoId, numeros, token) => {
     },
     body: JSON.stringify(bodyData),
   });
+
   if (!res.ok) {
-    throw new Error(`Error al apartar números: ${res.status}`);
+    const errorData = await res.json();
+    
+    throw new Error(errorData.error || `Error ${res.status}: No se pudieron apartar los números.`);
   }
+
   const data = await res.json();
   return data;
 }
+
 // Obtener boletos creados de un sorteo
 
 export const getBoletosPorSorteo = async (sorteoId) => {
